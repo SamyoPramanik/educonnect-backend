b79e60e590e64fc1e55b19e1ae1c80cf1d55fff2
Samyo Pramanik <samyopramanik2003@gmail.com>
2025-09-07 16:19:36 +0600

    LoginDto accepts email only

--- auth-service/src/main/java/com/educonnect/auth_service/dto/LoginReqDto.java
+++ auth-service/src/main/java/com/educonnect/auth_service/dto/LoginReqDto.java
@@ -1,9 +1,11 @@
 package com.educonnect.auth_service.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class LoginReqDto {
     @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email address")
     private String email;
 
     @NotBlank(message = "Password is required")
